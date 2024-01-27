3b8c6340f34aae30ace214f0e1312bfc03ebc88b
javohirtohirovich <javoergashev30@gmail.com>
2024-01-27 17:00:02 +0500

    Angular Create Footer,Header, Layout,Home components

--- WebTotalCommander/webtotalcommander.client/src/app/app-routing.module.ts
+++ WebTotalCommander/webtotalcommander.client/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"",
+  component: LayoutComponent,
+  children:[
+      {
+          path:"home",
+          component:HomeComponent
+      },
+      {
+          path:"",
+          redirectTo:"/home",
+          pathMatch:"full"
+      }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WebTotalCommander/webtotalcommander.client/src/app/app.module.ts
+++ WebTotalCommander/webtotalcommander.client/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { GridModule } from '@progress/kendo-angular-grid';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    HeaderComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
