f9bf5939bb61714914943f2754dc08782e6c52ed
javohirtohirovich <javoergashev30@gmail.com>
2024-02-08 18:25:26 +0500

    Code Formated app.module.ts

--- WebTotalCommander/webtotalcommander.client/src/app/app.module.ts
+++ WebTotalCommander/webtotalcommander.client/src/app/app.module.ts
@@ -1,49 +1,77 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { GridModule } from '@progress/kendo-angular-grid';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//For (ngModel,ngForm ...)
+import { FormsModule } from '@angular/forms';
+
+//For (ngIf,ngFor ...)
+import { CommonModule } from '@angular/common';
+
+//Router
 import { AppRoutingModule } from './app-routing.module';
+
+//Main Component
 import { AppComponent } from './app.component';
+
+//Components I created
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LayoutComponent } from './components/layout/layout.component';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 
-////////////
-import { ToastrModule} from 'ngx-toastr';
+//Toastr
+import { ToastrModule } from 'ngx-toastr';
+
+//Kendo Ui
+import { GridModule } from '@progress/kendo-angular-grid';
 import { NavigationModule } from "@progress/kendo-angular-navigation";
 import { ButtonsModule } from "@progress/kendo-angular-buttons";
 import { DialogsModule } from "@progress/kendo-angular-dialog";
-
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideToastr } from 'ngx-toastr';
 import { SVGIconModule } from '@progress/kendo-angular-icons';
 import { InputsModule } from "@progress/kendo-angular-inputs";
 import { PagerModule } from "@progress/kendo-angular-pager";
 import { LabelModule } from '@progress/kendo-angular-label';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    HomeComponent,
-    HeaderComponent,
-    LayoutComponent
-  ],
-  imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule,GridModule, FormsModule,CommonModule, NavigationModule,
-    ToastrModule.forRoot({
-      timeOut: 2000,
-    positionClass: 'toast-top-right',
-    preventDuplicates: true,
-    }),BrowserAnimationsModule,ButtonsModule,SVGIconModule,DialogsModule,InputsModule,FormsModule,PagerModule,LabelModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        //Main Component
+        AppComponent,
+        //Components I created
+        FooterComponent,
+        HomeComponent,
+        HeaderComponent,
+        LayoutComponent
+    ],
+    imports: [
+        HttpClientModule,
+        BrowserAnimationsModule,    
+        BrowserModule,
+        
+        //Router
+        AppRoutingModule,
+        //For (ngModel,ngForm ...)
+        FormsModule, 
+        //For (ngIf,ngFor ...)
+        CommonModule,     
+        //Kendo
+        NavigationModule,
+        PagerModule, 
+        LabelModule,
+        ButtonsModule, 
+        SVGIconModule, 
+        DialogsModule, 
+        InputsModule, 
+        GridModule,
+        //Toastr
+        ToastrModule.forRoot({
+            timeOut: 2000,
+            positionClass: 'toast-top-right',
+            preventDuplicates: true,
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
