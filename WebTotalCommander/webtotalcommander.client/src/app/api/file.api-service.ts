19b1ba35795dbf5388ba65b118e0e77f07b779fb
javohirtohirovich <javoergashev30@gmail.com>
2024-02-08 16:43:52 +0500

    Code Format folder.api-service.ts and file.api-service.ts

--- WebTotalCommander/webtotalcommander.client/src/app/api/file.api-service.ts
+++ WebTotalCommander/webtotalcommander.client/src/app/api/file.api-service.ts
@@ -1,49 +1,56 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable, inject } from "@angular/core";
-import { FileCreateModel } from "./models/file/file.create-model";
-import { Observable } from "rxjs";
-import { FileDeleteModel } from "./models/file/file.delete-model";
-import { FileEditModel } from "./models/file/file.edit-model";
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { FileCreateModel } from './models/file/file.create-model';
+import { Observable, catchError } from 'rxjs';
+import { FileDeleteModel } from './models/file/file.delete-model';
+import { FileEditModel } from './models/file/file.edit-model';
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class FileApiService {
     //Variable HttpClient Inject
     private client: HttpClient = inject(HttpClient);
 
-    //Variable Backend URL 
-    private url: string = "https://localhost:7251/api/file";
+    //Variable Backend URL
+    private url: string = 'https://localhost:7251/api/file';
 
-    //Upload File
+    //Function (request) Upload File
     public addFile(file: FileCreateModel): Observable<any> {
         const formData: FormData = new FormData();
-        formData.append("FilePath", file.filePath);
-        formData.append("File", file.file!);
+        formData.append('FilePath', file.filePath);
+        formData.append('File', file.file!);
         return this.client.post(this.url, formData);
     }
 
-    //Download File
+    //Function (request) Download File
     public downloadFile(filePath: string): Observable<any> {
         return this.client.get(`${this.url}?filePath=${filePath}`, {
-            responseType: 'blob'
+            responseType: 'blob',
         });
     }
 
-    //Delete File
+    //Function (request) Delete File
     public deleteFile(fileDeleteModel: FileDeleteModel): Observable<any> {
         return this.client.delete(this.url, { body: fileDeleteModel });
     }
 
-    //Get Txt File (for edit)
+    //Function (request) Get Txt File (for edit)
     public getTxtFile(filePath: string): Observable<any> {
-        return this.client.get(`${this.url}/text?file_path=${filePath}`, {
-            responseType: 'blob'
-        })
+        return this.client
+            .get(`${this.url}/text?file_path=${filePath}`, {
+                responseType: 'blob',
+            }).pipe(catchError((error) => {
+                    throw error;
+                })
+            );
     }
 
-    //Edit Txt File
+    //Function (request) Txt File
     public editTxtFile(fileEditModel: FileEditModel): Observable<any> {
         const formData: FormData = new FormData();
-        formData.append("file", fileEditModel.file!);
-        return this.client.put(`${this.url}/text?filePath=${fileEditModel.filePath}`, formData);
+        formData.append('file', fileEditModel.file!);
+        return this.client.put(
+            `${this.url}/text?filePath=${fileEditModel.filePath}`,
+            formData
+        );
     }
 }
--- WebTotalCommander/webtotalcommander.client/src/app/api/folder.api-service.ts
+++ WebTotalCommander/webtotalcommander.client/src/app/api/folder.api-service.ts
@@ -7,43 +7,47 @@ import { FolderGetAllModel } from "./models/common/folder.getall-model";
 
 @Injectable({ providedIn: "root" })
 export class FolderApiService {
+
+    //Variable HttpClient Inject
     private client: HttpClient = inject(HttpClient);
+    //Variable Backend URL
     private url: string = "https://localhost:7251/api/folder"
 
-    //Folder GetAll
+    //Function (request) GetAll Folders and Files
     public getAllFolder(folderPath: string,skip:number,take:number): Observable<FolderGetAllModel> {
+        //If Folder Path empty
         if (folderPath.length === 0) {
             return this.client.get<FolderGetAllModel>(`${this.url}?skip=${skip}&take=${take}`).pipe(
                 catchError((error) => {
-                    console.error("Error in getFolders:", error);
                     throw error;
                 })
             );
         }
+        //If Folder Path full
         else {
             return this.client.get<FolderGetAllModel>(`${this.url}?folder_path=${folderPath}&pageNumber=${skip}&pageSize=${take}`).pipe(
                 catchError((error) => {
-                    console.error("Error in getFolders:", error);
                     throw error;
                 })
             );
         }
-
     }
 
-    //Create Folder
+    //Function (request) Create Folder
     public addFolder(folder: FolderCreateModel): Observable<any> {
         return this.client.post(this.url, folder)
     }
 
-    //Download Folder Zip
+    //Function (request) Download Folder Zip
     public downloadFolderZip(folderPath:string,folderName:string):Observable<any>
     {
+        //If Folder Path empty
         if(folderPath.length===0){
             return this.client.get(`${this.url}/zip?folderName=${folderName}`,{
                 responseType: 'blob'
             });
         }
+        //If Folder Path full
         else{
             return this.client.get(`${this.url}/zip?folderPath=${folderPath}&folderName=${folderName}`, {
                 responseType: 'blob'
@@ -52,8 +56,7 @@ export class FolderApiService {
        
     }
 
-    //Delete Folder
-
+    //Function (request) Delete Folder
     public deleteFolder(folder:FolderDeleteModel):Observable<any>{
         return this.client.delete(this.url,{body:folder})
     }
